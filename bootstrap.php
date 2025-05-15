147167b359ba9a323c0a9a19d3822c91897c5a92
Pippone Francone <americo.cherubini2003@gmail.com>
2025-05-15 21:00:30 +0200

    aggiornato il boostrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,26 +1,40 @@
 <?php
-/* codice preso da https://www.doctrine-project.org/projects/doctrine-orm/en/3.3/reference/configuration.html
-    -americo
-*/
 
-// bootstrap.php
-require_once "vendor/autoload.php";
+require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/config/config.php');
 
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMSetup;
+use Doctrine\ORM\ORMSetup;;
 
-$paths = ['/path/to/entity-files'];
-$isDevMode = false;
+/**
+ * Resistuisce un instanza di EntityManager, connessa al database configurato in config.php.
+ */
+function getEntityManager() : EntityManager
+{
+    $entityManager = null;
 
-// the connection configuration
-$dbParams = [
-    'driver'   => 'pdo_mysql',
-    'user'     => 'root',
-    'password' => '',
-    'dbname'   => 'foo',
-];
+    if ($entityManager === null)
+    {
 
-$config = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
-$connection = DriverManager::getConnection($dbParams, $config);
-$entityManager = new EntityManager($connection, $config);
+        # set up configuration parameters for doctrine.
+        # Make sure you have installed the php7.0-sqlite package.
+        $connectionParams = array(
+            'dbname' => DB_NAME,
+            'user' => DB_USER,
+            'password' => DB_PASS,
+            'host' => DB_HOST,
+            'driver' => 'pdo_mysql',
+        );
+        
+        // percorso alla cartella con le classi entità
+        $paths = [__DIR__."/sportZone/entity"];
+        $isDevMode = false;
+        
+        $config = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
+        $connection = DriverManager::getConnection($connectionParams, $config);
+        $entityManager = new EntityManager($connection, $config);
+    }
+
+    return $entityManager;
+}
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+//Database Connection
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'test');
+define('DB_USER', 'pierino');
+define('DB_PASS', 'ciro');
