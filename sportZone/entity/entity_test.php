0bf617d9788eb119482453f4be1d1a40f2db7ae6
Pippone Francone <americo.cherubini2003@gmail.com>
2025-05-16 15:19:39 +0200

    testing
    metà lavoro sulle classi entity

--- sportZone/entity/entity_test.php
+++ sportZone/entity/entity_test.php
@@ -1,24 +1,53 @@
 <?php
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
 
 /**
- * File di prova .php, eliminatelo appena iniziate a scrivere codice vero
+ * Questo è un template per le classi entity; per creare altre classi entity, partite da questa. Aggiungete
+ * metodi e attributi necessari (i #[..] sono per le specifiche del database, li aggiungo io)
  * - americo
  */
+#[ORM\Entity]
+#[ORM\Table(name: "TabellaEntityTest")]
+class EntityTemplate
+{   
+  /*
+   * Tutti gli attributi vanno dichiarati come privati, e poi vanno aggiunti i metodi di set/get per ogniuno.
+   * La sintassi è:  private <tipo> $<nome>;
+   */
 
+    #[ORM\Id]
+    #[ORM\GeneratedValue("AUTO")]
+    #[ORM\Column(type: "integer")]
+    private int $id;
 
-class Fruit {
+    #[ORM\Column(type: "integer", nullable: false)]
+    private int $property1 = 0;
 
-  // Properties
-  public $name;
-  public $color;
+    /* Si mette ?<tipo> nel caso in cui il valore può essere nullo.
+      * E infatti non è richiesto un valore di inizializzazione (il valore è null per default)
+      */
+    #[ORM\Column(type: "string", nullable: true)]
+    private ?string $property2;
 
-  // Methods
-  function set_name($name) {
-    $this->name = $name;
-  }
-  
-  function get_name() {
-    return $this->name;
-  }
+    
+    #[ORM\OneToMany(mappedBy: )]
+    private ?ArrayCollection $stringhe;
+
+    public function setProperty1(int $value) {
+      $this->property1 = $value;
+    }
+
+    public function getProperty1(): int {
+      return $this->property1;
+    }
+
+    public function setProperty2(string $value) {
+      $this->property2 = $value;
+    }
+
+    public function getProperty2(): ?string {
+      return $this->property2;
+    }
 }
-?>
