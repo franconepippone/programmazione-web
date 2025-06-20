a98708675855ada741b5009425a37925d909c2f7
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-20 22:26:12 +0200

    Update FField.php

--- sportZone/foundation/FField.php
+++ sportZone/foundation/FField.php
@@ -1,1 +1,15 @@
+<?php
+require_once __DIR__ . "/../entity/EField.php";
 
+class FField {
+
+    /**
+     * Retrieve a Field by ID
+     *
+     * @param int $id
+     * @return EField|null
+     */
+    public static function retrieveFieldById(int $id): ?EField {
+        return FEntityManager::getInstance()->retriveObj(EField::class, $id);
+    }
+}
