f979184bdd634b11187928995a6078bb9a516a2a
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-20 22:30:24 +0200

    Update FPersistentManager.php

--- sportZone/foundation/FPersistentManager.php
+++ sportZone/foundation/FPersistentManager.php
@@ -3,8 +3,7 @@
 require_once __DIR__ . "/FUser.php";
 require_once __DIR__ . "/FReservation.php";
 require_once __DIR__ . "/FField.php";
-require_once __DIR__ . "/FClient.php";
-require_once __DIR__ . "/FPaymentMethod.php";
+
 
 class FPersistentManager{
 
