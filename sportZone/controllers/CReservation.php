a3dda37897e3de6c92dcc6f1c94a302f2cc2d909
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-20 21:58:41 +0200

    Update CReservation.php

--- sportZone/controllers/CReservation.php
+++ sportZone/controllers/CReservation.php
@@ -4,8 +4,12 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 class CReservation{
 
   public static function createForm(){
+
+     if (!CUser::isLogged()) {
+            return;
+        }
     
-    $fields = FPersistentManager::getInstance()->retriveAll(EField::getEntity());
+    $fields = FPersistentManager::getInstance()->loadAllFields();
     $view = new VReservation();
     $view->showReservationForm($fields);
   
