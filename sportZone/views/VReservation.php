563a3620e08e02e66ab382dd785823a9aa205173
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-20 20:01:00 +0200

    Update VReservation.php

--- sportZone/views/VReservation.php
+++ sportZone/views/VReservation.php
@@ -1,1 +1,14 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
 
+class VReservation{
+
+    private $smarty;
+
+    public function __construct(){
+        $this->smarty = USmarty::getInstance();
+    }
+
+    public function showCreateForm() {
+        $this->smarty->display("reservation/create_form.tpl");
+    }
