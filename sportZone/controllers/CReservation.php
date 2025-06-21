eaa957c3ca5b4b55ce59af422e69013645ab47ec
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-21 11:44:35 +0200

    Update CReservation.php

--- sportZone/controllers/CReservation.php
+++ sportZone/controllers/CReservation.php
@@ -10,8 +10,18 @@ class CReservation{
         }
     
     $view = new VReservation();
+    
     //$fieldId = UHTTPMethods::get('fieldId');
+    //if (!$fieldId) {
+       // VError::show("ID del campo non specificato.");
+        //return;
+    
     //$field = FPersistentManager::getInstance()->retriveFieldById($fieldId);
+    //if (!$field) {
+       // VError::show("Campo non trovato.");
+        //return;
+
+    //$date = UHTTPMethods::get('date') ?? (new DateTime())->format('Y-m-d');
     $view->showReservationForm();
   
   }
