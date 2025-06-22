9ff2f510a31736a14a4d2893b3ce681b7409118c
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-22 13:37:02 +0200

    Update VEmployee.php

--- sportZone/views/VEmployee.php
+++ sportZone/views/VEmployee.php
@@ -1,1 +1,13 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
 
+class VEmployee{
+
+    private $smarty;
+
+    public function __construct(){
+        $this->smarty = USmarty::getInstance();
+    }
+
+  public function 
+}
