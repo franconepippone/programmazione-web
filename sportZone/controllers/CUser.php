4ad4d280a8f8dc49a98407b7991eca014f2b76ba
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-22 15:50:20 +0200

    Update CUser.php

--- sportZone/controllers/CUser.php
+++ sportZone/controllers/CUser.php
@@ -125,6 +125,14 @@ class CUser{
         header('Location: /user/login');
     }
 
+    /**
+ * This method checks if the current user is logged in and has the role of "employee".
+ * It initializes the session if needed, verifies the user session element,
+ * and confirms the user's role. If the user is not logged in or does not have
+ * the "employee" role, it redirects to the appropriate page (login or access denied).
+ * 
+ * @return bool Returns true if the user is logged in as employee, otherwise redirects and exits.
+ */
     public static function home(){
         if(CUser::isLogged()){
             $view = new VUser();
@@ -134,5 +142,28 @@ class CUser{
             $view->showHomePage($user->getFullName());
         }  
     }
+
+    public static function isEmployee(){
+
+    if(UCookie::isSet('PHPSESSID')){
+        if(session_status() == PHP_SESSION_NONE){
+            USession::getInstance();
+        }
+    }
+
+    if(!USession::isSetSessionElement('user')){
+        header('Location: /User/login');
+        exit;
+    }
+
+    $user = USession::getSessionElement('user');
+
+    if(!isset($user['role']) || $user['role'] !== 'employee'){
+        header('Location: /User/accessDenied');
+        exit;
+    }
+
+    return true;
+}
 
 }
