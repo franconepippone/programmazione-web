dc40c21ec2b5a227be140175d9a94f920249beba
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-22 11:44:27 +0200

    Update CReservation.php

--- sportZone/controllers/CReservation.php
+++ sportZone/controllers/CReservation.php
@@ -110,9 +110,9 @@ class CReservation{
     // Otherwise, show the finalize reservation page with data for user to choose payment method
     $view = new VReservation();
     $view->showFinalizeReservation( //passare i parametri
-        'fullName' => $fullName,
-        'date' => $date,
-        'time' => $time);
+        $fullName,
+        $date,
+        $time);
         //'field' => $field
        // ]);
  }
