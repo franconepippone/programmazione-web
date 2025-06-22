3fb62c266d1c715d6c4d6555c57ecd87457b5eb9
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-22 11:08:51 +0200

    Update FClient.php

--- sportZone/foundation/FClient.php
+++ sportZone/foundation/FClient.php
@@ -1,1 +1,15 @@
+<?php
+require_once __DIR__ . "/../entity/EClient.php";
 
+class FClient {
+
+    /**
+     * Retrieve a Client by ID
+     *
+     * @param int $id
+     * @return EClient|null
+     */
+    public static function getClientById(int $id): ?EClient {
+        return FEntityManager::getInstance()->retriveObj(EClient::class, $id);
+    }
+}
