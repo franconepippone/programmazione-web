0036f02514ed5716838dfa63fe04627ab0a98337
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-23 16:44:44 +0200

    Update FCourse.php

--- sportZone/foundation/FCourse.php
+++ sportZone/foundation/FCourse.php
@@ -2,3 +2,8 @@
 require_once __DIR__ . "/../entity/ECourse.php";
 
 class FCourse {
+
+  public static function saveCourse(ECourse $course): void {
+        FEntityManager::getInstance()->save($course);
+    }
+}
