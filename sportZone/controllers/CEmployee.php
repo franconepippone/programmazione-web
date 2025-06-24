2dd1bf0d3e1d6219a22ff3edba5744dc34bc80d6
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-24 15:59:34 +0200

    Update CEmployee.php

--- sportZone/controllers/CEmployee.php
+++ sportZone/controllers/CEmployee.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../../vendor/autoload.php";
 
 class CEmployee{
-
+/**
   public static function cancelReservation() {
      //  Check if user is logged in and is employee
       CUser::isLogged();
@@ -132,7 +132,7 @@ class CEmployee{
     $view = new VEmployee();
     $view->viewReservation($reservation);
  }
-
+**/
  public static function createCourseForm() {
     if (!CUser::isLogged()) {
         header("Location: /login");
