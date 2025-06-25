d2cf5a9f2406831c78229bb124b15ea54c0c4ef9
WithWithout04 <alice.cosenza@student.univaq.it>
2025-06-25 12:14:52 +0200

    Update VReservation.php

--- sportZone/views/VReservation.php
+++ sportZone/views/VReservation.php
@@ -19,6 +19,7 @@ class VReservation{
 
     public function showFinalizeReservation($fullName,$date,$time,$field){ 
         $fieldData = EField::fieldToArray($field);
+        $this->smarty->assign("fieldData", $fieldData);
         $this->smarty->assign("fullName", $fullName);
         $this->smarty->assign("fieldData", $fieldData);
         $this->smarty->assign("date", $date);
@@ -32,6 +33,7 @@ class VReservation{
 
     public function showCancelReservation($reservation) {
         $reservationData = EReservation::reservationToArray($reservation);
+        $this->smarty->assign("reservation", $reservationData);
         $this->smarty->display("reservation/cancel_reservation.tpl");
     }
 
